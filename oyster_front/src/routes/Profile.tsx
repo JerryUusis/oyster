7865a7a59520fabd7906bf3dc8a412a7c5bd44b3
JerryU <jerry.uusitalo@gmail.com>
2024-07-05 20:41:04 +0300

    Create initial styles

--- oyster_front/src/routes/Profile.tsx
+++ oyster_front/src/routes/Profile.tsx
@@ -1,10 +1,35 @@
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import LocationIcon from "../assets/location.svg?react";
 
 const Profile = () => {
   const theme = useTheme().palette.oysterColors;
   return (
-    <Box sx={{ backgroundColor: theme.pink }}>Profile</Box>
+    <Box>
+      <Box sx={{ backgroundColor: theme.pink, height: "1000px" }}>
+        <Box sx={{ backgroundColor: theme.lightOlive, display: "flex" }}>
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTJ8fGZhY2V8ZW58MHx8MHx8fDI%3D"
+            alt="user profile photo"
+            style={{
+              width: "100%",
+              height: "375px",
+              borderTopLeftRadius: "100%",
+              borderTopRightRadius: "100%",
+              objectFit: "cover",
+            }}
+          />
+        </Box>
+        <Typography
+          variant="h1"
+          sx={{ textAlign: "center", color: theme.darkBrown }}
+        >
+          User Name
+        </Typography>
+        <LocationIcon />
+      </Box>
+    </Box>
   );
 };
 
