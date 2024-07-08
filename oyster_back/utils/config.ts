903d38aa9e8445d0f35f87c26e58a1ee3eb9ad6c
JerryU <jerry.uusitalo@gmail.com>
2024-07-08 16:29:51 +0300

    Add google admin sdk default credentials

--- oyster_back/utils/config.ts
+++ oyster_back/utils/config.ts
@@ -3,3 +3,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const HOST = process.env.HOST;
+export const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS;
