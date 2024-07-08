a75077a88d2ff2b2bb1a3bf814fe40f05c676a61
JerryU <jerry.uusitalo@gmail.com>
2024-07-08 09:09:55 +0300

    Add DNS resolution

--- oyster_front/vite.config.ts
+++ oyster_front/vite.config.ts
@@ -2,6 +2,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import dns from "node:dns";
+
+dns.setDefaultResultOrder("verbatim"); // https://vitejs.dev/config/server-options.html#server-host
 
 // https://vitejs.dev/config/
 export default defineConfig({
