e1320eddb003a95a7c7ca92b27bc1d2d0399c9e2
JerryU <jerry.uusitalo@gmail.com>
2024-07-09 11:21:15 +0300

    Add google_application_credentials in variables

--- oyster_back/utils/config.ts
+++ oyster_back/utils/config.ts
@@ -3,4 +3,5 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const HOST = process.env.HOST;
-export const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+export const GOOGLE_APPLICATION_CREDENTIALS =
+  process.env.GOOGLE_APPLICATION_CREDENTIALS;
