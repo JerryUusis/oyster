804b68d353afca64435073a3481cd4883b5d7535
JerryU <jerry.uusitalo@gmail.com>
2024-07-09 15:24:24 +0300

    Create an endpoint for registering new users and adding records of them to 'users' collection

--- oyster_back/controllers/user.ts
+++ oyster_back/controllers/user.ts
@@ -1,26 +1,27 @@
 import express from "express";
-import { admin, firestore } from "../services/firebaseAdmin";
+import { createUserWithEmailAndPasword } from "../services/firestore";
 const user = express.Router();
 
 // register a new user
 user.post("/", async (request, response) => {
   const { email, password, username } = request.body;
   try {
-    const userRecord = await admin.auth().createUser({ email, password });
+    const userRecord = await createUserWithEmailAndPasword(
+      email,
+      password,
+      username
+    );
 
-    // Store the user data in Firestore
-    await firestore.collection("users").doc(userRecord.uid).set({
-      email: userRecord.email,
-      username: username,
-      createdAt: admin.firestore.FieldValue.serverTimestamp(),
-    });
-    // Mint a custom token for the user
-    const customToken = await admin.auth().createCustomToken(userRecord.uid);
-
-    response.status(201).send({ uid: userRecord.uid, token: customToken });
+    response
+      .status(201)
+      .json({
+        uid: userRecord.uid,
+        email: userRecord.email,
+        username: userRecord.username,
+      });
   } catch (error) {
-    console.error("Error creating custom token:", error);
-    response.status(500).send("Error creating custom token");
+    console.error(error);
+    response.status(500).json({ message: error });
   }
 });
 
