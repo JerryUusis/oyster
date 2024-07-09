3792f46a1ea41831aa35017be660034c2ddb8a82
JerryU <jerry.uusitalo@gmail.com>
2024-07-09 15:59:31 +0300

    Add error codes for auth

--- oyster_back/utils/middleware/middleware.ts
+++ oyster_back/utils/middleware/middleware.ts
@@ -1,7 +1,34 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 const unknownEndpoint = (_request: Request, response: Response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-export { unknownEndpoint };
+const errorHandler = (
+  error: any,
+  _request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // auth error codes
+  //https://firebase.google.com/docs/auth/admin/errors
+  if (error.code && error.code.startsWith("auth/")) {
+    switch (error.code) {
+      case "auth/invalid-email":
+        return response.status(400).json({ error: "invalid email format" });
+      case "auth/user-not-found":
+        return response.status(404).json({ error: "user not found" });
+      case "auth/email-already-exists":
+        return response.status(400).json({ error: "email already in use" });
+      case "auth/invalid-password":
+        return response.status(400).json({ error: "invalid password" });
+      case "auth/weak-password":
+        return response.status(400).json({ error: "weak password" });
+      default:
+        return response.status(500).json({ error: "internal server error" });
+    }
+  }
+  next(error);
+};
+
+export { unknownEndpoint, errorHandler };
