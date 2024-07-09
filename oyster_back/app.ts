a251e2d839e5d640c807d764c50911b5ad37651d
JerryU <jerry.uusitalo@gmail.com>
2024-07-09 16:04:58 +0300

    Add error handling middleware

--- oyster_back/app.ts
+++ oyster_back/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import "express-async-errors";
 import { morganSetup } from "./utils/middleware/morganConfig";
+import { unknownEndpoint, errorHandler } from "./utils/middleware/middleware";
 import userRouter from "./controllers/user";
 
 const app = express();
@@ -11,5 +13,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(morganSetup);
 app.use("/api/user", userRouter);
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 export default app;
