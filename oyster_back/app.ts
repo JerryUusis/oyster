34659bbd3da85f47105d65e448dc1ebc72cfeb68
JerryU <jerry.uusitalo@gmail.com>
2024-07-09 11:18:48 +0300

    Add userRouter

--- oyster_back/app.ts
+++ oyster_back/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { morganSetup } from "./utils/middleware/morganConfig";
+import userRouter from "./controllers/user";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors({ origin: "http://localhost:5173" }));
 app.use(helmet());
 app.use(express.json());
 app.use(morganSetup);
+app.use("/api/user", userRouter)
 
 export default app;
