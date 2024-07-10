bccc53c617359d66fa3dbd3b5bf49fd85c5273b8
JerryU <jerry.uusitalo@gmail.com>
2024-07-10 12:29:03 +0300

    Add endpoint for fetching a single user

--- oyster_back/controllers/user.ts
+++ oyster_back/controllers/user.ts
@@ -1,11 +1,24 @@
 import express from "express";
-import { createUserWithEmailAndPasword, getUsers } from "../services/firestore";
+import {
+  createUserWithEmailAndPasword,
+  getUsers,
+  getUserById,
+} from "../services/firestore";
 const user = express.Router();
 
 // get all users
 user.get("/", async (_request, response) => {
   const users = await getUsers();
   response.status(200).json(users);
+});
+
+// get user with id
+user.get("/:id", async (request, response) => {
+  const user = await getUserById(request.params.id);
+  if (user) {
+    response.status(200).json(user);
+  }
+  response.status(404).json({ error: "user not found" });
 });
 
 // register a new user
