32da77ddf5d50cc05c3ee08255c55fb68d437b41
JerryU <jerry.uusitalo@gmail.com>
2024-07-11 23:04:44 +0300

    Update config to include emulator variables

--- oyster_back/src/utils/config.ts
+++ oyster_back/src/utils/config.ts
@@ -5,3 +5,6 @@ export const PORT = process.env.PORT;
 export const HOST = process.env.HOST;
 export const GOOGLE_APPLICATION_CREDENTIALS =
   process.env.GOOGLE_APPLICATION_CREDENTIALS;
+export const FIRESTORE_EMULATOR_HOST = process.env.FIRESTORE_EMULATOR_HOST;
+export const FIREBASE_AUTH_EMULATOR_HOST =
+  process.env.FIREBASE_AUTH_EMULATOR_HOST;
