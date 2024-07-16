cd8f6b6d7b51c9491a595210fa4b445d366254ec
JerryU <jerry.uusitalo@gmail.com>
2024-07-16 14:01:51 +0300

    Conditionally set emulator .env varialbles for dev and test environments

--- oyster_back/src/services/firebaseAdmin.ts
+++ oyster_back/src/services/firebaseAdmin.ts
@@ -5,6 +5,12 @@ admin.initializeApp({
   credential: admin.credential.cert(GOOGLE_APPLICATION_CREDENTIALS as string),
 });
 
+// Use Firebase emulators for test and dev environments
+if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
+  process.env.FIRESTORE_EMULATOR_HOST = "127.0.0.1:8080";
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099";
+}
+
 const firestore = admin.firestore();
 
 const auth = admin.auth();
