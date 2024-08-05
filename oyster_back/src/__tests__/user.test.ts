4753692e926d979f1698de483cd5de124f33954c
JerryU <jerry.uusitalo@gmail.com>
2024-08-05 15:59:51 +0300

    Add describe block for POST

--- oyster_back/src/__tests__/user.test.ts
+++ oyster_back/src/__tests__/user.test.ts
@@ -1,20 +1,29 @@
-import { initializeUsers, clearUsers, getNonExistingUid } from "./testHelper";
+import {
+  initializeUsers,
+  clearUsers,
+  getNonExistingUid,
+  generatePassword,
+} from "./testHelper";
 import { getUsers } from "../services/firestore";
 import app from "../app";
 import supertest from "supertest";
+import { UserInterface } from "../utils/types";
 const api = supertest(app);
+const BASE_URL = "/api/user";
 
 describe("API tests", () => {
   beforeEach(async () => {
     await clearUsers();
     await initializeUsers();
   });
+
   afterEach(async () => {
     await clearUsers();
   });
+
   describe("GET", () => {
     test("initial length of users is 3", async () => {
-      const result = await api.get("/api/user").expect(200);
+      const result = await api.get(BASE_URL).expect(200);
       expect(result.body.length).toBe(3);
     });
     test("UID as parameter returns single user", async () => {
@@ -25,8 +34,27 @@ describe("API tests", () => {
     });
     test("nonexsting uid parameter returns 404", async () => {
       const nonExistingUid = await getNonExistingUid();
-      const result = await api.get(`/api/user/${nonExistingUid}`).expect(404);
+      const result = await api.get(`${BASE_URL}/${nonExistingUid}`).expect(404);
       expect(result.body.error).toBe("user not found");
+    });
+  });
+
+  describe("POST", () => {
+    test("should increase the length of users collection by 1", async () => {
+      const initialUsers = await api.get(BASE_URL);
+      const newUser: UserInterface = {
+        email: "newuseremail@gmail.com",
+        username: "New User",
+        password: generatePassword(),
+      };
+      await api.post(BASE_URL).send(newUser).expect(201);
+      const usersAtEnd = await api.get(BASE_URL);
+      expect(usersAtEnd.body.length).toBe(initialUsers.body.length + 1);
+      expect(
+        usersAtEnd.body.find(
+          (user: UserInterface) => user.username === newUser.username
+        )
+      ).not.toBe(undefined);
     });
   });
 });
