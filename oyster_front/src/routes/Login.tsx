9cc20bdaae4130fe5899e20b0f54bd659a205ea5
JerryU <jerry.uusitalo@gmail.com>
2024-08-06 13:09:09 +0300

    Initialize Login form

--- oyster_front/src/routes/Login.tsx
+++ oyster_front/src/routes/Login.tsx
@@ -1,5 +1,34 @@
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100vw",
+          height: "calc(100vh - 56px)",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+        }}
+      >
+        <Typography variant="h2">Login</Typography>
+        <TextField label="Email" onChange={(e) => setEmail(e.target.value)} />
+        <TextField
+          label="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
