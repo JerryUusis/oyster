f058701529883d23067010543c6f3cf341ae09d6
JerryU <jerry.uusitalo@gmail.com>
2024-08-07 12:53:19 +0300

    Initiate testing for login router

---
+++ oyster_back/src/__tests__/login.test.ts
@@ -0,0 +1,64 @@
+import { clearUsers, generatePassword } from "./testHelper";
+import supertest from "supertest";
+import app from "../app";
+import { UserInterface } from "../utils/types";
+import { createUserWithEmailAndPasword } from "../services/firestore";
+import { initializeApp } from "firebase/app";
+import {
+  signInWithEmailAndPassword,
+  connectAuthEmulator,
+  getAuth,
+} from "firebase/auth";
+const api = supertest(app);
+const BASE_URL = "/api/login";
+
+// Emulate firebase client application for testing
+const firebaseConfig = {
+  apiKey: process.env.TEST_FIREBASE_API_KEY,
+};
+
+initializeApp(firebaseConfig);
+const authClient = getAuth();
+connectAuthEmulator(authClient, "http://localhost:9099", {
+  disableWarnings: true,
+});
+
+describe("login router", () => {
+  let idToken: string;
+  let user: FirebaseFirestore.DocumentData;
+
+  beforeEach(async () => {
+    await clearUsers();
+    const password = generatePassword();
+    const newUser: UserInterface = {
+      username: "test user",
+      email: "testuser@gmail.com",
+      password,
+    };
+    const { email, username } = newUser;
+
+    // Create a new user
+    user = await createUserWithEmailAndPasword(email, password, username);
+
+    // Sign in with previously created user
+    const userCredential = await signInWithEmailAndPassword(
+      authClient,
+      user.email,
+      password
+    );
+    idToken = await userCredential.user.getIdToken();
+  });
+  afterEach(async () => {
+    await clearUsers();
+  });
+  afterAll(async () => {
+    await clearUsers();
+  });
+  test("login with idToken should return user data and customToken", async () => {
+    const response = await api.post(BASE_URL).send({ idToken }).expect(200);
+    expect(response.body).toEqual({
+      ...user,
+      customToken: response.body.customToken,
+    });
+  });
+});
