097560de7793fd273e17a9e56f2d5f44a0dc382e
JerryU <jerry.uusitalo@gmail.com>
2024-08-12 13:45:10 +0300

    Initialize Redux store

--- oyster_front/src/main.tsx
+++ oyster_front/src/main.tsx
@@ -4,15 +4,19 @@ import App from "./App.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./utils/theme/theme.ts"
+import theme from "./utils/theme/theme.ts";
+import { Provider } from "react-redux";
+import store from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <CssBaseline />
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
     </Router>
   </React.StrictMode>
 );
