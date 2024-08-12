47e50448a7b88a8886a7250126b2f99320d6fa94
JerryU <jerry.uusitalo@gmail.com>
2024-08-12 10:37:30 +0300

    Import Register.tsx

--- oyster_front/src/App.tsx
+++ oyster_front/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Root from "./routes/Root";
 import Login from "./routes/Login";
 import Profile from "./routes/Profile";
+import Register from "./routes/Register";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Root />}>
         <Route path="/login" element={<Login />} />
         <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/register" element={<Register />}/>
       </Route>
     </Routes>
   );
