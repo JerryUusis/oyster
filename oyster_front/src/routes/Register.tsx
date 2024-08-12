ffa7bbf8e74d4a2e8ac4a4d79528bd9b6f82a946
JerryU <jerry.uusitalo@gmail.com>
2024-08-12 10:41:20 +0300

    Add registration logic

--- oyster_front/src/routes/Register.tsx
+++ oyster_front/src/routes/Register.tsx
@@ -1,10 +1,28 @@
-import { useTheme } from "@mui/material/styles";
+import { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import { registerNewUser } from "../services/registerService";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      await registerNewUser({ username, email, password });
+      setUsername("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      // TODO: Implement feedback on failed sign in
+      console.log(error);
+    }
+  };
   return (
     <Box
       sx={{
@@ -16,20 +34,40 @@ const Register = () => {
         width: "100vw",
       }}
     >
-      <Typography variant="h2" mb="1rem">Register</Typography>
+      <Typography variant="h2" mb="1rem">
+        Register
+      </Typography>
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          gap:"1rem"
+          gap: "1rem",
         }}
+        component={"form"}
+        onSubmit={handleSubmit}
       >
-        <TextField label="Username" />
-        <TextField label="Email" />
-        <TextField label="Password" type="password" />
-        <Typography>Already have an account? Go to <Link to="/login">login</Link>.</Typography>
+        <TextField
+          label="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="submit">Register</Button>
+        <Typography>
+          Already have an account? Go to <Link to="/login">login</Link>.
+        </Typography>
       </Box>
     </Box>
   );
