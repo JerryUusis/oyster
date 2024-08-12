f20f32479e135f1d617380b0c54545f065ce5096
JerryU <jerry.uusitalo@gmail.com>
2024-08-12 14:42:18 +0300

    Remove unused code

--- oyster_front/src/store/alertSlice.ts
+++ oyster_front/src/store/alertSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { AlertHandlerState } from "../utils/types";
 
 const alertSlice = createSlice({
