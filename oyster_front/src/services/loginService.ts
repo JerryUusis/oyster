ed9db6feb19c16cc34a7f022384b00433cffd254
JerryU <jerry.uusitalo@gmail.com>
2024-08-13 13:54:55 +0300

    Update error handling of loginService

--- oyster_front/src/services/loginService.ts
+++ oyster_front/src/services/loginService.ts
@@ -1,17 +1,20 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 const BASE_URL = "http://localhost:3001/api/login";
 
-const loginWithIdToken = async (idToken: string) => {
+const loginWithEmailAndPassword = async (email: string, password: string) => {
   try {
-    const response = await axios.post(BASE_URL, null, {
-      headers: {
-        Authorization: `Bearer ${idToken}`,
-      },
-    });
+    const response = await axios.post(BASE_URL, { email, password });
     return response.data;
   } catch (error) {
-    return error;
+    if (isAxiosError(error) && error.response?.data.error) {
+      switch (error.response.data.error) {
+        case "invalid username or password":
+          throw new Error("Wrong password");
+        case "user not found":
+          throw new Error("User not found");
+      }
+    }
   }
 };
 
-export { loginWithIdToken };
+export { loginWithEmailAndPassword };
