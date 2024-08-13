353864a9e971dccacf980b5c8377963a2ff6ecf6
JerryU <jerry.uusitalo@gmail.com>
2024-08-13 14:02:06 +0300

    Refactor custom token based login and error handling

--- oyster_front/src/routes/Login.tsx
+++ oyster_front/src/routes/Login.tsx
@@ -3,16 +3,12 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { useState } from "react";
-import {
-  signInWithEmailAndPassword,
-  signInWithCustomToken,
-} from "firebase/auth";
+import { signInWithCustomToken } from "firebase/auth";
 import { auth } from "../services/firebaseAuthentication";
-import { loginWithIdToken } from "../services/loginService";
+import { loginWithEmailAndPassword } from "../services/loginService";
 import AlertHandler from "../components/AlertHandler";
 import { useDispatch } from "react-redux";
 import { setAlert } from "../store/alertSlice";
-import { FirebaseError } from "firebase/app";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -31,54 +27,26 @@ const Login = () => {
           })
         );
       }
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const idToken = await userCredential.user.getIdToken(); // https://firebase.google.com/docs/auth/users#auth_tokens
-      const response = await loginWithIdToken(idToken);
+      const response = await loginWithEmailAndPassword(email, password);
 
+      // Sign in with the custom token received from the backend
       const loginResult = await signInWithCustomToken(
         auth,
         response.customToken
-      ); // Sign in with the custom token received from the backend
+      );
+
       // If custom token sign-in was succesful, store signed in user data and custom token in local storage
       if (loginResult.user) {
         const currentUser = { ...response };
         localStorage.setItem("loggedUser", JSON.stringify(currentUser));
       } else {
-        console.log("Custom token sign-in failed");
+        throw new Error("Custom token sign-in failed");
       }
     } catch (error) {
-      // Handle errors if SignInWithEmailAndPassword fails
-      if (error instanceof FirebaseError) {
-        const errorMessage = handleLoginErrorMessages(error.code);
-        dispatch(
-          setAlert({
-            severity: "error",
-            message: errorMessage,
-          })
-        );
+      if (error instanceof Error) {
+        dispatch(setAlert({ severity: "error", message: error.message }));
       }
-      setEmail("");
       setPassword("");
-    }
-  };
-
-  // List of auth error codes https://firebase.google.com/docs/reference/js/auth#autherrorcodes
-  const handleLoginErrorMessages = (errorCode: string): string => {
-    switch (errorCode) {
-      case "auth/invalid-email":
-        return "The email address is not valid";
-      case "auth/user-not-found":
-        return "No user found with this email";
-      case "auth/wrong-password":
-        return "The password is incorrect";
-      case "auth/network-request-failed":
-        return "Network error. Please check your internet connection and try again";
-      default:
-        return "An unknown error occurred. Please try again";
     }
   };
 
