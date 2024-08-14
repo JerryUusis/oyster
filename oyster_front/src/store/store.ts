7215c592081001c7d97eabc93be31f832490657a
JerryU <jerry.uusitalo@gmail.com>
2024-08-14 11:00:56 +0300

    Import userReducer

--- oyster_front/src/store/store.ts
+++ oyster_front/src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import alertReducer from "./alertSlice";
+import userReducer from "./userSlice";
 
 const store = configureStore({
-    reducer: {
-        alert: alertReducer
-    }
-})
+  reducer: {
+    alert: alertReducer,
+    user: userReducer,
+  },
+});
 
 export default store;
