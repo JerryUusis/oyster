405d9ae61dd9dfa4f5d09b40df3d3430cb1b7da4
JerryU <jerry.uusitalo@gmail.com>
2024-08-15 13:23:31 +0300

    Add getUserFromLocalStorage and refactor initial state

--- oyster_front/src/store/userSlice.ts
+++ oyster_front/src/store/userSlice.ts
@@ -1,14 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, Dispatch } from "@reduxjs/toolkit";
+import { UserState, UserObject } from "../utils/types";
+
+// State is either UserObject or null
 
 const userSlice = createSlice({
   name: "user",
-  initialState: {},
+  initialState: null as UserState,
   reducers: {
     setUser: (state, action) => {
       return action.payload;
     },
   },
 });
+
+export const getUserFromLocalStorage = () => (dispatch: Dispatch) => {
+  const userJSONString = localStorage.getItem("loggedUser");
+  if (userJSONString) {
+    const userObject: UserObject = JSON.parse(userJSONString);
+    dispatch(setUser(userObject));
+  }
+};
 
 export const { setUser } = userSlice.actions;
 export default userSlice.reducer;
