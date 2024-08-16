ce046bd9be37fcf097bcef070c4e3e4d64b1423b
JerryU <jerry.uusitalo@gmail.com>
2024-08-16 23:30:52 +0300

    Rename imports

--- oyster_front/__tests__/Header.test.tsx
+++ oyster_front/__tests__/Header.test.tsx
@@ -1,11 +1,11 @@
 import Header from "../src/components/Header";
 import { cleanup, screen } from "@testing-library/react";
-import renderWithTheme from "./utils/renderWithTheme";
+import { renderWithThemeAndProviders } from "./utils/renderWithTheme";
 
 describe("<Header />", () => {
   const header = <Header />;
   beforeEach(() => {
-    renderWithTheme(header);
+    renderWithThemeAndProviders(header);
   });
   afterEach(() => {
     cleanup();
@@ -15,7 +15,7 @@ describe("<Header />", () => {
     const header = screen.getByTestId("header");
     const profileButton = screen.getByTestId("profile-button");
     const menuButton = screen.getByTestId("menu-button");
-    
+
     expect(header).toBeVisible();
     expect(profileButton).toBeVisible();
     expect(menuButton).toBeVisible();
