76d4451db76322d64cde0870a9e05f19ec77fbfa
JerryU <jerry.uusitalo@gmail.com>
2024-08-20 11:12:14 +0300

    Add testAlertMessageAndColour

--- e2e/tests/mobile/testHelper.ts
+++ e2e/tests/mobile/testHelper.ts
@@ -1,4 +1,5 @@
-import { Page } from "@playwright/test";
+import { Page, Locator, expect } from "@playwright/test";
+import AlertHandlerComponent from "../utils/alertHandlerHelper";
 
 const RESET_URL =
   "http://localhost:8080/emulator/v1/projects/oyster-b6c36/databases/(default)/documents";
@@ -18,6 +19,26 @@ const loginUser = async (page: Page) => {
   await loginButton.click();
 };
 
+const testAlertMessageAndColour = async (
+  alertHandler: Locator,
+  message: string,
+  alertColour: string
+) => {
+  await alertHandler.waitFor({ state: "visible" });
+  await expect(alertHandler).toBeVisible();
+  await expect(alertHandler).toHaveText(message);
+  // Returns the return value of pageFunction.
+  // https://playwright.dev/docs/api/class-jshandle#js-handle-evaluate
+  // getComputedStyles returns an object containing the values of all CSS properties of an element
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+  const backgroundColor = await alertHandler.evaluate(
+    (element) => getComputedStyle(element).backgroundColor
+  );
+  expect(backgroundColor).toBe(alertColour);
+  alertHandler.waitFor({ state: "hidden" });
+  await expect(alertHandler).not.toBeVisible();
+};
+
 const clearUsers = async () => {
   try {
     const response = await fetch(RESET_URL, { method: "DELETE" });
@@ -27,6 +48,4 @@ const clearUsers = async () => {
   }
 };
 
-
-
-export { loginUser, clearUsers };
+export { loginUser, clearUsers, testAlertMessageAndColour };
