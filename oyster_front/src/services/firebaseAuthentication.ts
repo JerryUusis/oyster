86b0528119cc1a00c9ec4c5c07670ee9fd0c519e
JerryU <jerry.uusitalo@gmail.com>
2024-08-26 14:19:21 +0300

    Update condition for using auth emulator

--- oyster_front/src/services/firebaseAuthentication.ts
+++ oyster_front/src/services/firebaseAuthentication.ts
@@ -2,7 +2,9 @@ import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey:
+    import.meta.env.VITE_FIREBASE_API_KEY ||
+    import.meta.env.VITE_TEST_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
@@ -17,7 +19,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
 // Use emulators if using development environment
-if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+if (import.meta.env.DEV) {
   connectAuthEmulator(auth, "http://localhost:9099");
 }
 
