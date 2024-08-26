53e0a148d7e5088fe74fd294c5d3aa108e904784
JerryU <jerry.uusitalo@gmail.com>
2024-08-26 14:30:53 +0300

    Update config

--- oyster_front/src/services/firebaseAuthentication.ts
+++ oyster_front/src/services/firebaseAuthentication.ts
@@ -2,9 +2,7 @@ import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:
-    import.meta.env.VITE_FIREBASE_API_KEY ||
-    import.meta.env.VITE_TEST_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
