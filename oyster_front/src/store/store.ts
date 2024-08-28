5f0aeb332e53498aab942034d75c04eace62dcd8
JerryU <jerry.uusitalo@gmail.com>
2024-08-28 15:51:03 +0300

    Import countriesReducer

--- oyster_front/src/store/store.ts
+++ oyster_front/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import alertReducer from "./alertSlice";
 import userReducer from "./userSlice";
+import countriesReducer from "./countrySlice"
 
 const store = configureStore({
   reducer: {
     alert: alertReducer,
     user: userReducer,
+    countries: countriesReducer
   },
 });
 
