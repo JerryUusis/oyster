fff30f518c6fef050e57df598c25408cf005fd2e
JerryU <jerry.uusitalo@gmail.com>
2024-08-29 10:52:14 +0300

    Fix imports

--- oyster_front/__tests__/utils/renderWithTheme.tsx
+++ oyster_front/__tests__/utils/renderWithTheme.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"; // ReactNode is type for React element
 import { render, RenderResult } from "@testing-library/react"; // RenderResult is a type for object that render method returns
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import theme from "../../src/utils/theme/theme";
+import { theme } from "../../src/utils/theme/theme";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "../../src/store/store";
