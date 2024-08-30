a48b70134d100f0fb7f67d41c25a69830c80f603
JerryU <jerry.uusitalo@gmail.com>
2024-08-30 11:53:42 +0300

    Renamed DrawerMenuItem from MenuItem

---
+++ oyster_front/src/components/DrawerMenuItem.tsx
@@ -0,0 +1,47 @@
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material/SvgIcon/SvgIcon";
+import { Link } from "react-router-dom";
+
+export interface DrawerMenuItemProps {
+  itemName: string;
+  menuIcon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+    muiName: string;
+  };
+  path: string;
+  toggleDrawerVisible?: () => void;
+}
+
+const DrawerMenuItem = ({
+  itemName,
+  menuIcon: MenuIcon,
+  path,
+  toggleDrawerVisible,
+}: DrawerMenuItemProps) => {
+  return (
+    <Box>
+      <ListItem>
+        <ListItemButton
+          component={Link}
+          to={path}
+          onClick={toggleDrawerVisible}
+        >
+          <ListItemIcon>{MenuIcon && <MenuIcon />}</ListItemIcon>
+          <ListItemText>{itemName}</ListItemText>
+          <ListItemIcon sx={{ display: "flex", justifyContent: "flex-end" }}>
+            <ChevronRightRoundedIcon />
+          </ListItemIcon>
+        </ListItemButton>
+      </ListItem>
+      <Divider variant="middle" />
+    </Box>
+  );
+};
+
+export default DrawerMenuItem;
