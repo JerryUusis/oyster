d8f89aeb37b8d8579da201c1d1ecf9e88c8c8a8d
JerryU <jerry.uusitalo@gmail.com>
2024-12-05 10:47:15 +0200

    Add logo and offset Outlet

--- oyster_front/src/routes/Root.tsx
+++ oyster_front/src/routes/Root.tsx
@@ -1,13 +1,20 @@
+/// <reference types="vite-plugin-svgr/client" />
+import Logo from "../assets/logo.svg?react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Box from "@mui/material/Box";
 
 const Root = () => {
   return (
-    <Box>
+    <>
       <Header />
-      <Outlet />
-    </Box>
+      {/* Offset the bottom menu bar */}
+      {/* This has to implemented more dynamically in the future*/}
+      <Box pb="56px" position={"relative"}>
+        <Logo style={{ position: "absolute", top: 0, left: 0 }} />
+        <Outlet />
+      </Box>
+    </>
   );
 };
 
