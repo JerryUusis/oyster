2aa566a3c99fbf246b75de7ef4b40d8981085003
JerryU <jerry.uusitalo@gmail.com>
2024-12-05 15:31:23 +0200

    Update styles

--- oyster_front/src/components/SettingsMenuItem.tsx
+++ oyster_front/src/components/SettingsMenuItem.tsx
@@ -3,12 +3,19 @@ import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material/SvgIcon/SvgIcon";
+import { useTheme } from "@mui/material";
 
 interface SettingsMenuItemProps {
   settingName?: string;
   currentValue?: string;
   buttonLabel?: string;
   onClickFunction?: () => void | Promise<void>;
+  icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+    muiName: string;
+  };
 }
 
 const SettingsMenuItem = ({
@@ -16,14 +23,21 @@ const SettingsMenuItem = ({
   currentValue,
   buttonLabel,
   onClickFunction,
+  icon: Icon,
 }: SettingsMenuItemProps) => {
+  const oysterColors = useTheme().palette.oysterColors;
   return (
     <Box>
       <ListItem>
+        {Icon ? (
+          <ListItemIcon sx={{ color: oysterColors.darkBrown }}>
+            <Icon />
+          </ListItemIcon>
+        ) : null}
         <ListItemText primary={settingName} secondary={currentValue} />
         <Button
           variant="text"
-          sx={{ textDecoration: "underline" }}
+          sx={{ textDecoration: "underline", color: oysterColors.darkBrown }}
           onClick={onClickFunction}
         >
           {buttonLabel}
