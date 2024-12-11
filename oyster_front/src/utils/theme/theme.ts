7e0e2ee601e567bfa413c692df079efba8554e51
JerryU <jerry.uusitalo@gmail.com>
2024-12-11 09:58:21 +0200

    Update palette object

--- oyster_front/src/utils/theme/theme.ts
+++ oyster_front/src/utils/theme/theme.ts
@@ -2,7 +2,6 @@ import { createTheme, useTheme } from "@mui/material/styles";
 
 // Use module augmentation to add new variables to Theme
 // Access the custom theme using: import { useTheme } from "@mui/material/styles"
-// Then define theme in a variable like this: const theme = useTheme().palette.oysterColors
 
 declare module "@mui/material/styles" {
   interface Palette {
@@ -33,6 +32,15 @@ declare module "@mui/material/styles" {
 
 const theme = createTheme({
   palette: {
+    primary: {
+      main: "#341B11",
+    },
+    secondary: {
+      main: "#4B4D41",
+    },
+    error: {
+      main: "#FF6E34",
+    },
     oysterColors: {
       pink: "#C68E9D",
       lightPink: "#DDC3CA",
@@ -55,8 +63,10 @@ const theme = createTheme({
     },
   },
   components: {
+    // Implement default background color once themes are set for the project using MuiCssBaseline, styleOverrides, body
     MuiTypography: {
-      styleOverrides: { // https://mui.com/material-ui/customization/theme-components/
+      styleOverrides: {
+        // https://mui.com/material-ui/customization/theme-components/
         root: ({ theme }) => ({
           color: theme.palette.oysterColors.darkBrown,
         }),
@@ -65,9 +75,10 @@ const theme = createTheme({
   },
 });
 
+// This hook allows you to access theme colours
 const useOysterPalette = () => {
   const theme = useTheme();
   return theme.palette.oysterColors;
-}
+};
 
 export { theme, useOysterPalette };
