c0b2c065ada600e1a873030065d11c3c5cc76965
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-03 14:31:38 +0200

    Add updateUser function

--- oyster_front/src/services/registerService.ts
+++ oyster_front/src/services/registerService.ts
@@ -1,5 +1,5 @@
 import axios, { isAxiosError } from "axios";
-import { NewUserObject } from "../utils/types";
+import { NewUserObject, UserObject } from "../utils/types";
 import { HOST } from "../env_config";
 const BASE_URL = `http://${HOST()}:3001/api/user`;
 
@@ -16,4 +16,17 @@ const registerNewUser = async (newUserObject: NewUserObject) => {
   }
 };
 
-export { registerNewUser };
+const updateUser = async (newUserObject: UserObject) => {
+  try {
+    const response = await axios.put(`${BASE_URL}/${newUserObject.uid}`, newUserObject);
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response?.status === 400) {
+      throw new Error(error.response.data.error);
+    } else if (isAxiosError(error) && error.code === "ERR_NETWORK") {
+      throw new Error("Network error. Please try again later.");
+    }
+  }
+};
+
+export { registerNewUser, updateUser };
