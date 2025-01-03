dc9d98582633873d6002bf42b8c6d149a4e455c4
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-03 16:24:44 +0200

    Add setInput function and import getLanguages

--- oyster_front/src/components/SettingsMenuItem.tsx
+++ oyster_front/src/components/SettingsMenuItem.tsx
@@ -5,13 +5,17 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Input from "@mui/material/Input";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import { SvgIconTypeMap } from "@mui/material/SvgIcon/SvgIcon";
 import { useOysterPalette } from "../utils/theme/theme";
 import { useState } from "react";
-import { UserObject } from "../utils/types";
+import { CountryObject, UserObject } from "../utils/types";
 import { useAppSelector, useAppDispatch } from "../store/hooks";
 import { setUser } from "../store/userSlice";
+import { getLanguages } from "../utils/library";
 
 interface SettingsMenuItemProps {
   settingName: string;
@@ -43,7 +47,11 @@ const SettingsMenuItem = ({
   const [newValue, setNewValue] = useState(currentValue);
 
   const user = useAppSelector((state) => state.user);
+  const countries = useAppSelector(
+    (state) => state.countries
+  ) as CountryObject[];
   const dispatch = useAppDispatch();
+  const languages = getLanguages(countries);
 
   const oysterPalette = useOysterPalette();
 
@@ -63,7 +71,7 @@ const SettingsMenuItem = ({
         case "Location":
           keyToUpdate = settingName.toLowerCase() as keyof UserObject;
           break;
-          // TODO: Add an array of strings containing all of the languages with an autocomplete menu
+        // TODO: Add an array of strings containing all of the languages with an autocomplete menu
         case "Spoken languages":
           keyToUpdate = "languages" as keyof UserObject;
           break;
@@ -82,6 +90,27 @@ const SettingsMenuItem = ({
     }
   };
 
+  // Choose the right input for the right type
+  const setInput = () => {
+    if (
+      settingName === "Username" ||
+      settingName === "Email" ||
+      settingName === "Location" ||
+      settingName === "Theme color"
+    ) {
+      return (
+        <Input onChange={(e) => setNewValue(e.target.value)} value={newValue} />
+      );
+    } else if (settingName === "Spoken languages") {
+      return (
+        <Autocomplete
+          options={languages}
+          renderInput={(params) => <TextField {...params} label="Languages" />}
+        />
+      );
+    }
+  };
+
   return (
     <Box>
       <ListItem>
@@ -92,16 +121,7 @@ const SettingsMenuItem = ({
         ) : null}
         <ListItemText
           primary={settingName}
-          secondary={
-            !isEditing ? (
-              currentValue
-            ) : (
-              <Input
-                onChange={(e) => setNewValue(e.target.value)}
-                value={newValue}
-              />
-            )
-          }
+          secondary={!isEditing ? currentValue : setInput()}
         />
         <Button
           variant="text"
