40bf4d78a5b181fc4d9019d9de0a5310e0e94114
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-03 14:32:20 +0200

    Add updateUserObject function

--- oyster_front/src/utils/library.ts
+++ oyster_front/src/utils/library.ts
@@ -1,8 +1,19 @@
 import { signOut, getAuth } from "firebase/auth";
+import { updateUser } from "../services/registerService";
+import { UserObject } from "./types";
 
 const signUserOut = async () => {
   const auth = getAuth();
   await signOut(auth);
 };
 
-export { signUserOut };
+const updateUserObject = async (
+  userObject: UserObject,
+  keyToUpdate: keyof UserObject,
+  newValue: string
+) => {
+  const newUserObject = { ...userObject, [keyToUpdate]: newValue };
+  return await updateUser(newUserObject);
+};
+
+export { signUserOut, updateUserObject };
